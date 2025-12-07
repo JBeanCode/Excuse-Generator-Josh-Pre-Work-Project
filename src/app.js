fdd464427ca395b5b9a25192d253245eacbc84a4
JBeanCode <js02bean@gmail.com>
2025-12-07 23:27:42 +0000

    Prework excuse generator

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+console.log("JS is running!");
+
 import "bootstrap";
 import "./style.css";
 
@@ -7,5 +9,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+let who = ['The cat ', 'Batman ', 'A pink flamingo ', 'Jennifer Lopez '];
+let action = ['bit ', 'peed on ', 'flew off with ', 'shredded '];
+let what = ['my homework ', 'my phone ', 'my computer ', 'the car '];
+let when = ['before the class.', 'when I was sleeping.', 'while I was exercising.', 'during my lunch.', 'while I was watching Star Wars.'];
+
+let randomWho = who[Math.floor(Math.random() * who.length)];
+
+let randomAction = action[Math.floor(Math.random() * action.length)];
+
+let randomWhat = what[Math.floor(Math.random() * what.length)];
+
+let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+let excuse = `My excuse: ${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+
+document.getElementById("excuse").innerHTML = excuse;
 };
